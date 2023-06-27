40efed7a7bcf796513e64e86f978aac1f9698cc8
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-06-27 14:17:38 +0530

    Final Pushes

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -41,7 +41,7 @@ router.post("/create-user", upload.single("file"), async (req, res, next) => {
       avatar: fileUrl,
     };
     const activationToken = createActivationToken(user);
-    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+    const activationUrl = `https://shop-app-s8q6.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
