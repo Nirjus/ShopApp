b6a3d2250021f97d625d772b4fbb3dc46c8991e1
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-06-29 02:58:33 +0530

    push backend url in src

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://shop-app-backend.vercel.app/api/v2";
+export const server = "http://52.66.196.220:8000/api/v2";
 
-export const backend_url = "https://shop-app-backend.vercel.app/"
+export const backend_url = "http://52.66.196.220:8000/"
