eafe85bd12b159e7206056ec0d402e029f987ec5
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-06-29 14:45:24 +0530

    new commmit for socket

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -9,7 +9,7 @@ import { AiOutlineSend } from "react-icons/ai";
 import { GrGallery } from "react-icons/gr";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
-const ENDPOINT = "http://localhost:5000/";
+const ENDPOINT = "https://socket-ecommerce-moyl.onrender.com/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"]});
 
 const DashboardMessages = () => {
