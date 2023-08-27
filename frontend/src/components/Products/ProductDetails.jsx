77bda80e2de9a64520516796f0d648ecc0a5529d
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-08-27 22:11:05 +0530

    seller pic updated again

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -18,7 +18,6 @@ import { addTocart } from "../../redux/actions/cart";
 import { toast } from "react-toastify";
 import Ratings from "./Ratings";
 import axios from "axios";
-import img from "../../Assets/product5.jpg";
 
 const ProductDetails = ({ data }) => {
   const { cart } = useSelector((state) => state.cart);
@@ -28,6 +27,7 @@ const ProductDetails = ({ data }) => {
   const [count, setCount] = useState(1);
   const [click, setClick] = useState(false);
   const [select, setSelect] = useState(0);
+  const [newAvatar, setNewAvatar] = useState({});
   const navigate = useNavigate();
   const dispatch = useDispatch();
  
@@ -39,6 +39,19 @@ const ProductDetails = ({ data }) => {
       setClick(false);
     }
   }, [data, wishlist]);
+  useEffect(() => {
+    const id = data.shop._id;
+    axios
+      .get(`${server}/shop/get-shop-info/${id}`)
+      .then((res) => {
+        setNewAvatar(res.data.shop);
+       
+      })
+      .catch((error) => {
+        console.log(error);
+       
+      });
+  },[])
 
   const incrementCount = () => {
     setCount(count + 1);
@@ -201,7 +214,7 @@ const ProductDetails = ({ data }) => {
                 <div className="flex items-center pt-8">
                   <Link to={`/shop/preview/${data?.shop._id}`}>
                     <img
-                      src={`${data?.shop?.avatar?.url}` ? `${data?.shop?.avatar?.url}` : img}
+                      src={newAvatar.avatar?.url}
                       alt=""
                       className=" w-[50px] h-[50px] rounded-full mr-2"
                     />
@@ -233,6 +246,7 @@ const ProductDetails = ({ data }) => {
             products={products}
             totalReviewsLength={totalReviewsLength}
             averageRating={averageRating}
+            newAvatar={newAvatar}
           />
           <br />
           <br />
@@ -247,6 +261,7 @@ const ProductDetailsInfo = ({
   products,
   totalReviewsLength,
   averageRating,
+  newAvatar,
 }) => {
   const [active, setActive] = useState(1);
   return (
@@ -343,7 +358,7 @@ const ProductDetailsInfo = ({
             <Link to={`/shop/preview/${data.shop._id}`}>
               <div className="flex items-center">
                 <img
-                   src={`${data?.shop?.avatar?.url}` ? `${data?.shop?.avatar?.url}` : img}
+                   src={newAvatar.shop?.url}
                   alt=""
                   className=" w-[50px] h-[50px] rounded-full"
                 />
