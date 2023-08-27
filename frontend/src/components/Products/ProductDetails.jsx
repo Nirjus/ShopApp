33f68a32b507687c64b411764c34bfcde9386b0c
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-08-27 14:10:51 +0530

    shop avatar fixing again

--- frontend/src/components/Products/ProductDetails.jsx
+++ frontend/src/components/Products/ProductDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import styles from "../../styles/style";
 import {
   AiFillHeart,
