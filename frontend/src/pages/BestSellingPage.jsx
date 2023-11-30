23447c401935f06214b9ab9e6fcfa04ab74b976a
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2023-11-30 20:45:56 +0530

    change in bestSelling page

--- frontend/src/pages/BestSellingPage.jsx
+++ frontend/src/pages/BestSellingPage.jsx
@@ -32,7 +32,7 @@ const BestSellingPage = () => {
         <br />
         <div className={`${styles.section}`}>
           <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-12">
-            {data && data.map((i, index) => <ProductCard data={i} key={index} />)}
+            {data && data.slice(startIndex, lastIndex).map((i, index) => <ProductCard data={i} key={index} />)}
           </div>
             <div className="">
               <Pagination 
